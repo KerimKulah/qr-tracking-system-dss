8af4be5db991236ab9d2d7b241bb0ee6abd74e16
Kerim Külah <106034152+KerimKulah@users.noreply.github.com>
2024-10-22 21:19:52 +0300

    first commit

---
+++ qr-tracking-system-dss-be/src/main/java/com/mk/qr_tracking_system_dss/QrTrackingSystemDssApplication.java
@@ -0,0 +1,13 @@
+package com.mk.qr_tracking_system_dss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrTrackingSystemDssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrTrackingSystemDssApplication.class, args);
+	}
+
+}
---
+++ qr-tracking-system-dss-be/src/test/java/com/mk/qr_tracking_system_dss/QrTrackingSystemDssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mk.qr_tracking_system_dss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrTrackingSystemDssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
