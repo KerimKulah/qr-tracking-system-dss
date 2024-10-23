c26c48c4eb3d9aec02dae51cc63e310310a78106
Kerim Külah <106034152+KerimKulah@users.noreply.github.com>
2024-10-23 21:00:32 +0300

    Entityler ve enum eklendi.

--- qr-tracking-system-dss-be/src/main/java/com/mk/qr_tracking_system_dss/entity/BaseEntity.java
+++ qr-tracking-system-dss-be/src/main/java/com/mk/qr_tracking_system_dss/entity/BaseEntity.java
@@ -1,6 +1,7 @@
 package com.mk.qr_tracking_system_dss.entity;
 
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Data;
@@ -10,6 +11,6 @@ import lombok.Data;
 public abstract class BaseEntity {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 }
