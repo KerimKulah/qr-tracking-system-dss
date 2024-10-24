4845ab12035faf571df999459eb2fa732b43ff24
Kerim Külah <106034152+KerimKulah@users.noreply.github.com>
2024-10-24 20:44:27 +0300

    ..

--- qr-tracking-system-dss-be/src/main/java/com/mk/qr_tracking_system_dss/entity/Role.java
+++ qr-tracking-system-dss-be/src/main/java/com/mk/qr_tracking_system_dss/entity/Role.java
@@ -1,6 +1,9 @@
 package com.mk.qr_tracking_system_dss.entity;
 
+import com.mk.qr_tracking_system_dss.enums.RoleEnum;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.OneToMany;
 import lombok.Data;
 import java.util.List;
@@ -9,7 +12,8 @@ import java.util.List;
 @Entity
 public class Role extends BaseEntity {
 
-    private String roleName;
+    @Enumerated(EnumType.STRING)
+    private RoleEnum roleName;
 
     @OneToMany(mappedBy = "role")
     private List<User> users;
---
+++ qr-tracking-system-dss-be/src/main/java/com/mk/qr_tracking_system_dss/enums/RoleEnum.java
@@ -0,0 +1,6 @@
+package com.mk.qr_tracking_system_dss.enums;
+
+public enum RoleEnum {
+    ROLE_ADMIN,
+    ROLE_EMPLOYEE
+}
