00df3e2952789b7a235b8246ae6950b1767f7e2d
Kerim Külah <106034152+KerimKulah@users.noreply.github.com>
2024-12-04 19:55:14 +0300

    Update vite.config.js

--- qr-tracking-system-dss-fe/vite.config.js
+++ qr-tracking-system-dss-fe/vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // Yerel ağ üzerinden erişim için
+    port: 5173,
+  },
+});
