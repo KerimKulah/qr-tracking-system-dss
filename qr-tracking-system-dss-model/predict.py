1e590819103f5538112afe35eb16d557a642de71
Kerim Külah <106034152+KerimKulah@users.noreply.github.com>
2024-12-04 19:51:41 +0300

    Update predict.py

--- qr-tracking-system-dss-model/predict.py
+++ qr-tracking-system-dss-model/predict.py
@@ -31,4 +31,4 @@ def predict_category():
     return jsonify({"category": prediction[0]})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000, ssl_context=('vite.crt', 'vite.key'))
+    app.run(debug=True, host='0.0.0.0', port=5000)
